2d1efee06e7bcaad9a6505876f3b8e71f57b147a
RavgoYT <ravnoor.ghuman@gmail.com>
2026-04-06 19:17:47 -0700

    Update vite.config.js
    Trying to get Github Pages to work...

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // If deploying to https://username.github.io/repo-name/, set base to '/repo-name/'
   // If using a custom domain (e.g. colors.ravgo.dev), set base to '/'
-  base: '/',
+    base: '/ColorVisualizer/',
 });
